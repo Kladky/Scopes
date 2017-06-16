1ab80aa4fe461b9c413b21526e5ec2024166edb7
Kladky <kladky@users.noreply.github.com>
2017-06-16 13:56:10 -0400

    :grimacing: :grimacing: :grimacing:

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -8,11 +8,11 @@ export default function Campus (props) {
 
     return (
       <div>
-        <h2>Life: </h2>
+        <h2><i className="em em-sparkles"></i> Life: </h2>
         <p>{lifeTweets[0]}</p>
-        <h2>Love: </h2>
+        <h2><i className="em em-heart_eyes"></i> Love: </h2>
         <p>{loveTweets[0]}</p>
-        <h2>Career: </h2>
+        <h2><i className="em em-moneybag"></i> Career: </h2>
         <p>{careerTweets[0]}</p>
       </div>
     );
