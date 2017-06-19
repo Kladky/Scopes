09f68cfcc8a60df9058dfa83e0bf31fee28a2c61
Kladky <kladky@users.noreply.github.com>
2017-06-19 12:50:10 -0400

    database??

--- db/index.js
+++ db/index.js
@@ -5,13 +5,13 @@ const Sequelize = require('sequelize');
 const pkg = require('../package.json');
 
 const name = process.env.DATABASE_NAME || pkg.name;
-const connectionString = process.env.DATABASE_connectionString || `postgres://localhost:5432/${pkg.name}`;
+const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`;
 
 console.log(chalk.yellow(`Opening database connection to ${connectionString}`));
 
 // create the database instance that can be used in other database files
 const db = module.exports = new Sequelize(connectionString, {
-  logging: debug, // export DEBUG=sql in the environment to get SQL queries 
+  logging: debug, // export DEBUG=sql in the environment to get SQL queries
   native: true    // lets Sequelize know we can use pg-native for ~30% more speed (if you have issues with pg-native feel free to take this out and work it back in later when we have time to help)
 });
 
@@ -24,7 +24,7 @@ function sync(force=false, retries=0, maxRetries=5) {
   .then(ok => console.log(`Synced models to db ${connectionString}`))
   .catch(fail => {
     // Don't do this auto-create nonsense in prod, or
-    // if we've retried too many times. 
+    // if we've retried too many times.
     if (process.env.NODE_ENV === 'production' || retries > maxRetries) {
       console.error(chalk.red(`********** database error ***********`))
       console.error(chalk.red(`    Couldn't connect to ${connectionString}`))
